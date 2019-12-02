5ba86e364b80379bdeb208c46ca8b552a21bb0e0
marcio-melz <55721382+marcio-melz@users.noreply.github.com>
2019-12-02 20:40:08 -0300

    correção

--- app/src/main/java/com/Marcio/swipeapp/MainActivity.java
+++ app/src/main/java/com/Marcio/swipeapp/MainActivity.java
@@ -116,12 +116,16 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Função para resetar o index das pergunras, caso o index estiver fora do tamanho da lista de perguntas
      */
+
     private void resetIndexIfOut() {
-        if (screenIndex > (questions.size() - 1) || screenIndex < (questions.size() - 1)) {
+        if (screenIndex > (questions.size() - 1) ) {
             screenIndex = 0;
         }
-    }
 
+        if(screenIndex < 0){
+            screenIndex = (questions.size() - 1);
+        }
+    }
     /**
      * Função para voltar a pergunta
      */
